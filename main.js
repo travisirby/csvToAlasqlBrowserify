203ec2bc71b27a7f8791dc7b1b0029952f812eec
Travis Irby <travis.irby@gmail.com>
2018-02-13 09:25:15 -0500

    first

---
+++ main.js
@@ -0,0 +1,31 @@
+const csv = require('csvtojson');
+const alasql = require('alasql');
+const fileReaderStream = require('filereader-stream')
+
+document.getElementById('uploader').addEventListener('change', readFile, false);
+
+function readFile (evt) {
+    var files = evt.target.files;
+    var file = files[0];           
+    var reader = new FileReader();
+    var readStream = fileReaderStream(file);
+    alasql("CREATE TABLE example1 (a FLOAT)");
+
+      csv()
+      .fromStream(readStream)
+      .on('json',(jsonObj)=>{
+      	// combine csv header row and csv line to a json object
+      	// jsonObj.a ==> 1 or 4
+        // console.dir(jsonObj)
+        var units = jsonObj['Units Sold'];
+        alasql(`INSERT INTO example1 VALUES (${units})`);
+      })
+      .on('done',(error)=>{
+        if (error) console.log(error)
+      	console.log('end')
+        var res = alasql("SELECT SUM(a) from example1");
+        console.dir(res)
+      })
+
+    // reader.readAsText(file)
+}
